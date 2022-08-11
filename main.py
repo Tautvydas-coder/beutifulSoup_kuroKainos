b95c57e7d26d7d1485d13a69f2d6c6203241bec9
Tautvydas-coder <b7&*%13kHqvP>
2022-08-11 10:27:09 +0300

    csv two columns

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from lxml import html, etree
+from itertools import zip_longest
 import csv
 
 HEADERS = {
@@ -12,7 +13,7 @@ soup = BeautifulSoup(page.content, 'html.parser')
 # results = soup.find(class_='news').find('table', id="kainos").find_all('td')
 # print(results)
 
-body = soup.find('body').find('div').find('h1')
+body = soup.find('body').find('div').find('form').find('div').find('table')
 for link in body:
     print(link.get_text())
 
@@ -20,7 +21,8 @@ for link in body:
 
 root = html.fromstring(page.content)
 element = root.xpath('//html/body/div/div[2]/div[1]/div[4]/div[1]/h2')
-print(element[0].text)
+
+print(type(print(element[0].text)))
 
 tree = root.getroottree()
 result = root.xpath('/html/body/div//*')
@@ -28,20 +30,23 @@ result = root.xpath('/html/body/div//*')
 dom = etree.HTML(str(root))
 
 
-def write_to_file(result, dom):
+def write_to_file(result):
     with open('xpaths.csv', 'w') as file:
         for r in result:
             xpath = tree.getpath(r)
             full_xpath = '/' + xpath
             elements = root.xpath(full_xpath)
             content_text = elements[0].text
-            if content_text is None:
+            print(type(print(content_text)))
+            if content_text is None or content_text == "" or len(content_text) == 0:
                 content_text = str("None")
-                file.write(content_text+'\n')
+                file.write(content_text + ',')
+                file.write(full_xpath)
             else:
-                file.write(content_text+'\n')
-            file.write(full_xpath + '\n')
+                file.write(content_text + ',')
+                file.write(full_xpath)
+            file.write('\n')
 
 
 if __name__ == '__main__':
-    write_to_file(result, dom)
+    write_to_file(result)
