b6365e6708e3629a638514f073a6f1766960df9a
Tautvydas-coder <b7&*%13kHqvP>
2022-08-11 18:00:58 +0300

    added TODO

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 from lxml import html, etree
 
+# TODO pabandyti vietoje Label Element write whole html tag
+
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 '
                   'Safari/537.36'}
