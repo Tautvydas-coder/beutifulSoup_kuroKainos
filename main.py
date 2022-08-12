3620cbae92cae96d0ca7c9190b036ed2bdaa32a8
Tautvydas-coder <b7&*%13kHqvP>
2022-08-12 11:17:34 +0300

    refactoring

--- main.py
+++ main.py
@@ -1,61 +1,62 @@
 import requests
 from bs4 import BeautifulSoup
 from lxml import html, etree
+from page_info import page
 
-# TODO pabandyti vietoje Label Element write whole html tag
 
-HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 '
-                  'Safari/537.36'}
-page = requests.get(url='http://www.degalukainos.lt', headers=HEADERS)
-
-soup = BeautifulSoup(page.content, 'html.parser')
+# --------SOUP-----------
+# soup = BeautifulSoup(page.content, 'html.parser')
 # results = soup.find(class_='news').find('table', id="kainos").find_all('td')
 # print(results)
 # body = BeautifulSoup('<a class="pointer" href="http://www.degalukainos.lt" title="Degalų kainos Lietuvoje" xpath="1"><img src="/img/logo_2.gif" width="142" height="62" alt="logo"></a>','html.parser')
-# body = soup.find('body').find_all('div')
+# body = soup.find('body').find_all('img')
 # for link in body:
-#     print(link.get_text())
-#     print(link)
-#     print(link.img['alt'])
-
+# print(link.get_text())
+# print(link)
+# print(link.img['alt'])
 # print(soup)
 
-root = html.fromstring(page.content)
-element = root.xpath('//html/body/div/div[2]/div[2]/div/form/div/div[2]/table/tr[2]/td[10]/div[1]/img')
-elementy = ('//html/body/div/div[2]/div[2]/div/form/div/div[2]/table/tr[2]/td[10]/div[1]/img')
+# -------LXML-----------
+# element = root.xpath('//html/body/div/div[2]/div[2]/div/form/div/div[2]/table/tr[2]/td[10]/div[1]/img')
+# elementy = ('//html/body/div/div[2]/div[2]/div/form/div/div[2]/table/tr[2]/td[10]/div[1]/img')
 # last=elementy.split('/')
 # print(last[-1])
 # print(element)
 # print(element[0].attrib['title'])
 # print(type(print(element[0].text)))
-
-tree = root.getroottree()
-result = root.xpath('/html/body/div//*')
-
-dom = etree.HTML(str(root))
-
-
+# tree = root.getroottree()
+# results = root.xpath('/html/body/div//*')
+# dom = etree.HTML(str(root))
 # elementr = root.xpath('//html/body/div[1]/div[2]/div[2]/div/form/div/div[2]/table')
 # print(elementr[0].attrib['id'])
+# --------------------------------
+def fetch_page_content():
+    page_content = html.fromstring(page.content)
+    return page_content
 
-def write_to_file(result):
-    with open('xpaths.csv', 'w') as file:
-        for r in result:
-            xpath = tree.getpath(r)
+
+def fetch_root_tree(root):
+    web_tree = root.getroottree()
+    return web_tree
+
+
+def fetch_web_element_info(root):
+    web_elements = root.xpath('/html/body/div//*')
+    return web_elements
+
+
+def write_to_file(results, tree):
+    with open('degaluKainos_xpaths.csv', 'w') as file:
+        for result in results:
+            xpath = tree.getpath(result)
             full_xpath = '/' + xpath
-            if not (full_xpath.__contains__('script') or full_xpath.__contains__('plusone') or full_xpath.__contains__(
-                    'ins')):
+            if not (full_xpath.__contains__('script')):
                 elements = root.xpath(full_xpath)
-                # print(full_xpath)
                 content_text = elements[0].text
-                # print(content_text)
-                # print(type(print(content_text)))
                 last_elem = xpath.split('/')
-                # print(last_elem[-1])
                 if last_elem[-1] == 'img':
                     elem_title = elements[0].attrib['alt']
-                    file.write("alt: "+elem_title + ',')
+                    file.write("alt: " + elem_title + ',')
                     file.write(full_xpath)
                 elif last_elem[-1] == 'table':
                     file.write('Table' + ',')
@@ -69,16 +70,19 @@ def write_to_file(result):
                     file.write('Element: ' + elem_title + ',')
                     file.write(full_xpath)
                 elif content_text is None:
-                    file.write(str("Label Element ") + last_elem[-1] + ',')
+                    file.write("Label Element: " + last_elem[-1] + ',')
                     file.write(full_xpath)
                 elif content_text.isspace():
-                    file.write('Empty Label Element ' + last_elem[-1] + ',')
+                    file.write('Empty Label Element: ' + last_elem[-1] + ',')
                     file.write(full_xpath)
                 else:
-                    file.write(content_text + ',')
+                    file.write("Text: " + content_text + ',')
                     file.write(full_xpath)
                 file.write('\n')
 
 
 if __name__ == '__main__':
-    write_to_file(result)
+    root = fetch_page_content()
+    tree = fetch_root_tree(root)
+    results = fetch_web_element_info(root)
+    write_to_file(results, tree)
---
+++ page_info.py
@@ -0,0 +1,7 @@
+import requests
+
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 '
+                  'Safari/537.36'}
+
+page = requests.get(url='http://www.degalukainos.lt', headers=HEADERS)
