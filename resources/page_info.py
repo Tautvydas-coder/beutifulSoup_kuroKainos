f42f912812991a270f90240121e6f127a02435db
Tautvydas-coder <b7&*%13kHqvP>
2022-08-19 10:40:23 +0300

    refactor

--- resources/page_info.py
+++ resources/page_info.py
@@ -1,7 +1,9 @@
 import requests
+from resources.variables import *
 
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 '
                   'Safari/537.36'}
 
-page = requests.get(url='http://www.degalukainos.lt', headers=HEADERS)
+page = requests.get(url=URL, headers=HEADERS)
+# page = requests.get(url='https://www.messenger.com')
--- resources/variables.py
+++ resources/variables.py
@@ -1,3 +1,7 @@
 xpath_start = '/html/body/div//*'
 csv_name = 'outputs/Kainos_CSV_xpaths.csv'
 json_name = 'outputs/Kainos_JSON_xpaths.json'
+URL = 'http://www.degalukainos.lt'
+
+# csv_name = 'outputs/messenger_CSV_xpaths.csv'
+# json_name = 'outputs/messenger_JSON_xpaths.json'
